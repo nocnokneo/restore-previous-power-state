fa2ffb5e46279f1056dbba79575368f964d42cfd
Taylor Braun-Jones <taylor.braun-jones@ge.com>
2013-07-08 14:46:39 -0400

    Install scripts with execute permission!

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ install -m 755 -d \
     ${DESTDIR}/etc/pm/sleep.d \
     ${DESTDIR}/var/lib/pm-utils
 
-install -pm 644 restore-previous-power-state ${DESTDIR}/usr/bin/
-install -pm 644 log-previous-power-state ${DESTDIR}/etc/init.d/
+install -pm 755 restore-previous-power-state ${DESTDIR}/usr/bin/
+install -pm 755 log-previous-power-state ${DESTDIR}/etc/init.d/
 ln -s ../../init.d/log-previous-power-state ${DESTDIR}/etc/pm/sleep.d/00log-previous-power-state
 
 # Initialize the previous state for first use
