b973b31eb2ce114b92789710f86f62c40ce796fa
Taylor Braun-Jones <taylor.braun-jones@ge.com>
2013-07-08 14:11:50 -0400

    install.sh: Create required destination directories

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ set -e
 # DESTDIR provides a configurable system root to support RPM
 # builds. Default is just an empty string.
 
+install -m 755 -d \
+    ${DESTDIR}/usr/bin \
+    ${DESTDIR}/etc/init.d \
+    ${DESTDIR}/etc/pm/sleep.d \
+    ${DESTDIR}/var/lib/pm-utils
+
 install -pm 644 restore-previous-power-state ${DESTDIR}/usr/bin/
 install -pm 644 log-previous-power-state ${DESTDIR}/etc/init.d/
 ln -s ../../init.d/log-previous-power-state ${DESTDIR}/etc/pm/sleep.d/00log-previous-power-state
